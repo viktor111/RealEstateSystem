692a78e8d74e5d69a878e53a3057913e1be70bd9
viktor111 <swifrorlilko@gmail.com>
2022-07-28 18:53:31 +0300

    Initial commit

---
+++ RealEstateSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RealEstateSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
